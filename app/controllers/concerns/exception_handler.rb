2af6a2c13050da3c9a074d15e5f47f5ceb225fff
Ramon Hossein <hosseinramon@gmail.com>
2017-04-19 15:00:57 -0300

    authorize api request

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -5,13 +5,15 @@ module ExceptionHandler
   class AuthenticationError < StandardError; end
   class MissingToken < StandardError; end
   class InvalidToken < StandardError; end
+  class ExpiredSignature < StandardError; end
 
   included do
     # define custom handlers
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
     rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
@@ -20,12 +22,17 @@ module ExceptionHandler
 
   private
     # json response with message; Status code 422 - unprocessable entity
-    def four_twenty_two
+    def four_twenty_two(e)
       json_response({ message: e.message }, :unprocessable_entity)
     end
 
+    # json response with message; Status code 498 - invalid token
+    def four_ninety_eight(e)
+      json_response({ message: e.message }, :invalid_token)
+    end
+
     # josn response with message; Status code 401 - Unauthorized
-    def unauthorized_request
+    def unauthorized_request(e)
       json_response({ message: e.message }, :unauthorized)
     end
 end
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -12,8 +12,9 @@ class JsonWebToken
   def self.decode(token)
     # get payload; first index in decoded array
     body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
     # rescue from expiry exception
-  rescue JWT::ExpiredSignature, JWT::VerificationError => exp
+  rescue JWT::ExpiredSignature, JWT::VerificationError => e
     # raise custom error to be handled by custom handler
     raise ExceptionHandler::ExpiredSignature, e.message
   end
